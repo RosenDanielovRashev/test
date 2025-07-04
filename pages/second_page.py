fc2f79b62c2a2a45b0b85c9431e46b999663a1ac
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-04 18:16:13 +0300

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Втора Страница")
 st.write("Това е втората страница!")
 if st.button("Обратно към началото"):
-    st.switch_page("app.py")  # Връщане към главния файл
+    st.switch_page("appy.py")  # Връщане към главния файл
