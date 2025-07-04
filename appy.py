2cdd8bd904160fa10901a87e7e302e4dc5826c91
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-04 18:08:35 +0300

    Update appy.py

--- appy.py
+++ appy.py
@@ -2,9 +2,5 @@ import streamlit as st
 
 st.title("Главна страница")
 
-st.write("Налични страници:")
-for page in st._runtime._session_state._pages.keys():
-    st.write("-", page)
-
 if st.button("Отиди към second_page"):
     st.switch_page("second_page")
