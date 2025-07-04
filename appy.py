fbd27e4f668f0205f532dbb9b46b87ee600593dc
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-04 18:03:45 +0300

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,9 +1,4 @@
 # main_app.py
 import streamlit as st
 
-st.title("Главна страница")
-
-st.write("Натисни бутона, за да отидеш на втората страница.")
-
-if st.button("Отиди"):
-    st.switch_page("Втора страница")
+streamlit --version
