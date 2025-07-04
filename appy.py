fed5a3649ecbbb1aa79db352e3f8656414dcb8c1
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-04 17:57:11 +0300

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,29 +1,9 @@
+# main_app.py
 import streamlit as st
-import pandas as pd
 
-st.title("Примерен App за зареждане на CSV файлове с изолинии")
+st.title("Главна страница")
 
-# Зареждане на CSV файла с подходящи настройки
-@st.cache_data
-def load_fi_data():
-    try:
-        df = pd.read_csv("fi.csv", delimiter=';', encoding='utf-8')
-    except UnicodeDecodeError:
-        # Ако има проблем с utf-8, пробваме с latin1
-        df = pd.read_csv("fi.csv", delimiter=';', encoding='latin1')
-    return df
+st.write("Натисни бутона, за да отидеш на втората страница.")
 
-df_fi = load_fi_data()
-
-st.subheader("Данни от fi.csv:")
-st.dataframe(df_fi)
-
-# Параметър Fi, който потребителят въвежда
-fi_input = st.number_input("Въведи стойност на Fi:", value=0.0, step=0.1)
-
-st.write(f"Въведена стойност Fi: {fi_input}")
-
-# Примерна визуализация - може да добавиш още логика
-st.line_chart(df_fi[['fi']])
-
-# Тук можеш да добавиш следващата логика за обработка на данните
+if st.button("Отиди"):
+    st.switch_page("pages/second_page")
